9f07231e0817c65c32f4af3067495130b1a8f767
Sidnooma Christian KABORE <chriskabor@gmail.com>
2024-07-30 10:10:00 +0000

    added named slot

---
+++ slots/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <app-form>
+    <template v-slot:help>
+      <p class="text">This is some help text</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="email">
+      <input type="text" placeholder="username">
+      <input type="text" placeholder="password">
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </app-form>
+  <app-form></app-form>
+</template>
+
+<script>
+  import AppForm from "@/components/Form.vue"
+  export default {
+    name: "App", 
+    components: {
+      AppForm:AppForm, 
+    }
+  }
+</script>
---
+++ slots/src/components/Form.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+    <div class="help">
+      <slot name="help">No form to render</slot>
+    </div>
+    <div class="fields">
+      <slot name="fields"></slot>
+    </div>
+    <div class="buttons">
+      <slot name="buttons"></slot>
+    </div>
+  </form>
+  
+</template>
