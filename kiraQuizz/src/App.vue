16062ec5909f8cbd65dc045a687254673cf69c56
Sidnooma Christian KABORE <chriskabor@gmail.com>
2024-08-09 09:02:16 +0000

    added data to questions

--- kiraQuizz/src/App.vue
+++ kiraQuizz/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="ctr">
-    <questions v-if="questionsAnswered < questions.length"/>
+    <questions v-if="questionsAnswered < questions.length" 
+    :questions="questions"
+    :questionsAnswered="questionsAnswered"
+    @question-answered="questionAnswered"/>
     <result v-else/>
     <button type="button" class="reset-btn">Reset</button>
 </div>
@@ -18,6 +21,7 @@
   data(){
     return {
     questionsAnswered: 0,
+    totalCorrect:0,
     questions: [
         {
             q: 'What is 2 + 2?', 
@@ -94,6 +98,14 @@
         }
     ]
 }
+  }, 
+  methods:{
+    questionAnswered(is_correct){
+      if(is_correct){
+        this.totalCorrect++;
+      }
+      this.questionsAnswered++;
+    }
   }
 
  };
